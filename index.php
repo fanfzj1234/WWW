bbb49dcc2caa1e813de9e907a3029bb49e559e28
fanfzj1234 <fanfzj@163.com>
2014-07-29 19:24:25 +0800

    2014-7-29 new 上传2

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+  
+   define("APP_NAME","HOME");
+   define("APP_HOME","HOME");
+   define("APP_PATH", "./HOME/");
+   define("APP_DEBUG", true);
+   require './ThinkPHP/ThinkPHP.php';
+?>
+
