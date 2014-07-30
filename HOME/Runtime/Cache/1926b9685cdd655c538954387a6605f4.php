dfd1c7eed4c1121119ecd8e1d789193cb7650e06
fanfzj1234 <fanfzj@163.com>
2014-07-30 21:08:22 +0800

    缓存 2014/7/30

--- HOME/Runtime/Cache/1926b9685cdd655c538954387a6605f4.php
+++ HOME/Runtime/Cache/1926b9685cdd655c538954387a6605f4.php
@@ -1,10 +1,4 @@
-<?php if (!defined('THINK_PATH')) exit();?><!--
-	作者：范志俊
-	时间：2014-07-25
-	描述：主页面，拼接header，动态变换<main>内容，由index_main.js控制
--->
-<!-- 加载头文件 -->
-<!DOCTYPE html>
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="zh-CN">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -12,125 +6,304 @@
 <meta http-equiv="X-UA-Compatible" content="IE=Edge">
 <meta name="description" content="">
 <meta name="author" content="">
-
+<!-- Bootstrap & Snippet plugin core CSS-->
+<link href="http://cdn.bootcss.com/twitter-bootstrap/3.0.2/css/bootstrap.min.css" rel="stylesheet">
+<!-- Custom styles for this template -->
+<link href="http://www.bootcss.com/p/stickup/stickup.css" rel="stylesheet">
+<link rel="image_src" href="http://lirancohen.github.io/stickUp/images/stickUp.png">
 <title>爱拍课堂||ipaikt</title>
-<script>
-    var APP = "__APP__";
-	var IMG = "__IMG__";	
-</script>
-
-<!-- 新 jquery Javascript 文件 --> 
-<script type="text/javascript" src='http://code.jquery.com/jquery-2.1.1.js'></script>
-
-<!-- 新 Bootstrap 核心 CSS 文件 -->
-<link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap.min.css">
-
-<!-- 可选的Bootstrap主题文件（一般不用引入） -->
-<link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
-
-<!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
-<script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
-
-<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
-<script src="http://cdn.bootcss.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-
-<!-- 自定义  首页课程表 CSS 文件 -->
-<link rel="stylesheet" href="__CSS__/main_table.css" media="screen" type="text/css" />
-
-<!-- jquery-ui  CSS 文件 -->
-<link rel="stylesheet" href="__CSS__/jquery-ui.css" media="screen" type="text/css" />
-
-<!--font-awesome  标准文字库  CSS 文件 -->
-<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.css">
-
-<!--bootstrap  再编辑   CSS 文件 -->
-<link rel="stylesheet" href="__CSS__/bootstrap.css" media="screen" type="text/css"/>
-
-<!-- 主页 框架  CSS 文件 -->
-<link rel="stylesheet" href="__CSS__/index_main.css" media="screen" type="text/css" />
-
-<!-- 时间  Javascript 文件 -->
-<script type="text/javascript" src="__JS__/time.js"></script>
-
-<!-- 用户信息  Javascript 文件 -->
-<script type="text/javascript" src="__JS__/user_info.js"></script>
-
-
-<!-- 自定义 左侧 导航栏 跳转 Javascript 文件 -->
-<script>
-$(document).ready( function() {
-	$(".index_main").load(APP+"/Index/main");
-	$("#index").click(function(){
-		$(".index_main").load(APP+"/Index/main");
-	});			
-	$("#syllabus").click(function(){
-		$(".index_main").load(APP+"/Table/index");
-	});
-	$("#note").click(function(){
-		$(".index_main").load(APP+"/Note/index");
-	});
-	
-    $('body').on("click", ".larg div h3", function(){
-    if ($(this).children('span').hasClass('close')) {
-      $(this).children('span').removeClass('close');
-    }
-    else {
-      $(this).children('span').addClass('close');
-    }
-    /*$(this).parent().children('p').slideToggle(250);*/
-  });
-  
-  $('body').on("click", "nav ul li a", function(){
-    var title = $(this).data('title');
-    $('.title').children('h2').html(title);
-  });
-});
-
-</script>
-
 </head>
-<!-- url后面随机值，保证页面最新 -->
-<body
-ondragstart="return false;" 
-onload="if(window.location.href.indexOf('temp')==-1)window.location.href=window.location.href+'?temp='+Math.random().toString();bodyLoad();" 
-onpaste="return false;" 
-oncut="return false;"  
-oncopy="return false;" 
->
+<body>
+<body style="" data-find="_5" onload="if(window.location.href.indexOf('temp')==-1)window.location.href=window.location.href+'?temp='+Math.random().toString();bodyLoad();" >
+    <!-- START THE COVER -->
+    <div id="home" class="carousel" data-find="_4" style="margin-bottom: 20px;">
+      <div class="carousel-inner">
+        <div class="item active">
+          <div class="container">
+            <div class="carousel-caption">
+             	<img src="__IMG__/logo2.png"> 
+				<h1>爱拍课堂</h1>
+				<h3>一个 课程表和笔记相结合  APP应用</h3>
+				<p>这是一个快捷、方便的 课程表和笔记应用，它能让查询课程表，以及记录课堂笔记更方便。</p>
+				<p><a class="btn btn-large btn-success" href="__APP__/Index/index_main"><b>Go》》</b></a></p>
+            </div>
+          </div>
+        </div>
+    </div><!-- END COVER -->
+</div>
+    <!-- START THE NAVBAR -->
+    <div class="navbar-wrapper stuckMenu" style="margin-top:0px">
+      <div class="container">
+      <div class="navwrapper">
+        <div class="navbar navbar-inverse navbar-static-top">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">ipaikt</a>
+            </div>
+            <div class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li class="menuItem active"><a href="#home">首页</a></li>
+                <li class="menuItem"><a href="#features">特色</a></li>
+                <li class="menuItem"><a href="#updates">更新记录</a></li>
+                <li class="menuItem"><a href="#installation">建立更快捷</a></li>
+                <li class="menuItem"><a href="#one-pager">复习更方便</a></li>
+                <li class="menuItem"><a href="#extras">功能更多多</a></li>
+                <li class="menuItem"><a href="#contact">联系我们</a></li>    
+              </ul>
+            </div>
+          </div>
+        </div> 
+      </div><!-- End Navbar -->
+      
+      </div>
+    </div> <!-- END NAVBAR -->
 
-<span class='bckg'></span>
-<header>
-	
-  <h1>
-  	<img src="__IMG__/favicon.ico" id="logo" alt="爱拍课堂"/>
-    爱拍课堂
-  </h1>
-  <nav>
-    <ul>
-     <li>
-        <a data-title='首页' id="index">首页</a>
-      </li>
-      <li>
-        <a data-title='课程表' id="syllabus">课程表</a>
-      </li>
-      <li>
-        <a data-title='课堂笔记' id="note">课堂笔记</a>
-      </li>
-      <li>
-        <a data-title='我的资料' id="information">我的资料</a>
-      </li>      
-    </ul>
-     
-  </nav>
-   
-</header>
-<main  class="index_main"> 
-<article class='larg'>
-</article>
-</main>
-    
+    <!-- START THE CONTENT -->
+    <div id="contents" data-find="_3">  
+
+      <!-- START THE FEATURES -->   
+      <div id="features" class="featurette container marketing">
+        <!-- Three columns of text -->
+        <div class="row">
+        		<center><h4 style="margin-bottom:0px;margin-top:20px;"><b>Ipaikt APP</b></h4>
+        <h5 style="margin-top:5px;"><mute>v0.0.1 BETA</mute></h5></center>
+          <div class="col-lg-4">
+          
+            <img src="http://www.bootcss.com/p/stickup/images/simple.png">
+            <h2>建立更快捷</h2>
+            <p>课表秒导入，显示云同步</p>
+            <p>有图有真相，笔记轻松建</p>
+            <p>富媒体支持,内容更详实</p>
+            <p>便签轻松加，查找顶呱呱</p>
+            <p><a class="btn btn-default" href="#installation">详细内容 »</a></p>
+          </div><!-- /.col-lg-4 -->
+          <div class="col-lg-4">
+            <img src="http://www.bootcss.com/p/stickup/images/onepager.png">
+            <h2>复习更方便</h2>
+            <p>后台能识别，自动理知识</p>
+            <p>资源同步推，课程同步好</p>
+            <p>笔记共分享，人多力量大</p>
+            <p>评测帮不漏，考试有感觉</p>
+            <p><a class="btn btn-default" href="#one-pager">详细信息 »</a></p>
+          </div><!-- /.col-lg-4 -->
+          <div class="col-lg-4">
+            <img src="http://www.bootcss.com/p/stickup/images/opensource.png">
+            <h2>功能更多多</h2>
+            <p>老师来帮助，不挂有保障</p>
+            <p>平台版本全，终端任君选</p>
+            <p><a class="btn btn-default" href="#extras">详细信息 »</a></p>
+          </div><!-- /.col-lg-4 -->
+        </div><!-- /.row -->
+      </div> <!-- END FEATURES -->
+
+      <!-- START THE UPDATES -->
+      <div id="updates" class="row featurette">
+        <div class="col-lg-12">
+          <h2 class="featurette-heading">更新记录</h2>
+        </div>
+        <div class="col-lg-offset-1 col-lg-10">
+          <h3>APP 版发布！ <small><b><i>Nov 1st, 2014</i></b></small></h3>
+          <p>The first version of the <b><i>ipaikt</i></b> jQuery plugin has oficially been launched. This is still in Beta and there may be a few bugs, but they are will get sorted out in the coming weeks. Pleaes contact me if you experience any troubles. Check back often for updates on new features.
+          </p>
+        </div>
+        <div class="col-lg-offset-1 col-lg-10">
+          <h3>"One Pager" anchor functionality. <small><b><i>Oct 31st, 2013</i></b></small></h3>
+          <p>Decided to add anchor functionality so that as certain sections of a 'One Pager' style website, like this one, could trigger a CSS change as they're scrolled into view.  That way your users know where they are while they're scrolling through content.  More options are coming for this in the future.
+          </p>
+        </div>
+        <div class="col-lg-offset-1 col-lg-10">
+          <h3>In need of Margin... <small><b><i>Oct 30th, 2013</i></b></small></h3>
+          <p>When I initially made the plugin the goal was to make the element stick to the very top, but after deciding to make an official example page for it I realizd that I loved the look of a floating plugin.  So I added a little feature called <b><i>marginTop</i></b> to the mix.  This allows the element to be caught by it's margin rather than when it hits the top of the page.
+          </p>
+        </div>
+        <div class="col-lg-offset-1 col-lg-10">
+          <h3>stickUp is Alive! <small><b><i>Oct 29th, 2013</i></b></small></h3>
+          <p>I've decided to create a plugin out of necessity.  There are plenty of "sticky nav" jQuery plugins available out there, but none of them seem to work as smoothly as I'd like.  I decided to create my own and out of that stickUp was born! It's not perfect and has it's kinks, but they will be worked out over the coming weeks.
+          </p>
+        </div>
+      </div> <!-- END UPDATES -->
+
+      <!-- START THE INSTALLATION -->
+      <div id="installation" class="row featurette" data-find="_8">
+        <div class="col-md-7" data-find="_7">
+          <h2 class="featurette-heading">建立更快捷<br> <span class="text-muted">Set up more quickly.</span></h2>
+          <p class="lead">下载或从GitHub上面可控stickUp项目。解压到项目目录。请确保你已经正确引入了jQuery。如果需要了解jQuery，请参考<a href="http://learn.jquery.com/about-jquery/how-jquery-works/">jQuery 是如何工作的</a>。</p>
+          <p class="col-lg-offset-1"><a class="btn btn-large btn-primary" href="http://github.com/LiranCohen/stickUp">Visit GitHub</a><br><br></p>
+          <p class="lead">现在就可以将stickUp所包含的的javascript文件加载到你的页面中了。拷贝下面的代码，粘贴到页面底部。务必确保javascript文件的路径正确无误。
+          </p>
+          <xmp>
+            <script src="js/stickUp.min.js"></script>
+          </xmp>
+          <p class="lead">接下来，将下面的代码拷贝粘贴到页面底部，并且放到前面引入的stickUp文件下面。或者，你也可以将下面的代码放倒已经存在的javascript文件中（这个文件也必须在stickUp文件下面）。将 <i>'.navbar-wrapper'</i> 替换为目标元素的class名称。
+          </p>
+          <xmp>
+            <script type="text/javascript">
+              //initiating jQuery
+              jQuery(function($) {
+                $(document).ready( function() {
+                  //enabling stickUp on the '.navbar-wrapper' class
+                  $('.navbar-wrapper').stickUp();
+                });
+              });
+
+            </script>
+          </xmp>
+          <p class="lead">就这么简单！</p>  
+        </div>
+        <div class="col-md-5">
+        </div>
+      </div>  <!-- END INSTALLATION -->
+
+      <!-- START THE ONE-PAGER -->
+      <div id="one-pager" class="row featurette">
+        <div class="col-md-5">
+        </div>
+        <div class="col-md-7">
+          <h2 class="featurette-heading">复习更方便<span class="text-muted"><br>Review the more convenient</span></h2>
+          <p class="lead">如果你的页面布局为单页面导航，并希望导航条一直固定在顶部，让用户随时知道自己所处的位置，只需按照下面的步骤照着做就能实现。</p>
+          <p class="lead">
+            <b>第一步，</b> 收集页面上所有将要作为锚点的元素。<br>
+          </p>
+          <p class="lead">
+            <small><i>如果这些元素还没有被赋予 <b>"id="</b> 属性，请为其设置好。</i></small>
+          </p>
+          <p class="lead">
+            <b>接下来，</b> 修改 <i>stickUp</i> 插件的引用代码如下：
+          </p>
+          <xmp>
+            <script type="text/javascript">
+              //initiating jQuery  
+              jQuery(function($) {
+                $(document).ready( function() {
+                  //为 '.navbar-wrapper' class 启用stickUp插件
+                  $('.navbar-wrapper').stickUp({
+                                parts: {
+                                  0:'home',
+                                  1:'features',
+                                  2: 'news',
+                                  3: 'installation',
+                                  4: 'one-pager',
+                                  5: 'extras',
+                       
+                                  6: 'contact'
+                                },
+                                itemClass: 'menuItem',
+                                itemHover: 'active'
+                              });
+                });
+              });
+            
+            </script>
+          </xmp>
+          <p class="lead">
+            **上面的数字标注的是元素出现的顺序。
+            <br><br>
+            <b>itemClass:</b> 是每个导航菜单条目所对应的的class。<br>
+            <b>itemHover:</b> 是当菜单条目处于激活状态时所被赋予的class，用于修改其外观。
+          </p>
+        </div>
+      </div>  <!-- END ONE-PAGER -->
+
+      <!-- START THE EXTRAS -->
+      <div id="extras" class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">功能更多多<br><span class="text-muted">Function more</span></h2>
+          <p class="lead">
+            我正在继续完善这个插件。如果有建议或需求，请告诉我。
+          </p>
+          <p class="lead">
+            stickUp自己使用到了两个class，需要注意一下。<br><br>
+            <b>.stuckMenu</b>: 初始化插件时，这个class被添加到目标元素。<br>
+            <b>.isStuck</b>: 当stickUp对目标元素起作用时，此class将被添加到目标元素。反之则被移除。
+          </p>
+          <p class="lead">
+            目前stickUp仅提供了下面这一个而外参数，将来会加入更多。<br><br>
+            <b>marginTop</b>:
+            此参数将为目标元素设置上边距。可选值为 <i>'auto'</i>，这时，将会自动获取目标元素的上边距并应用到目标元素上；还可以设置为具体的高度。下面就是本页面导航条所用的代码。
+          </p>
+          <p>
+            </p><xmp>
+              <script type="text/javascript">
+                //initializing jQuery
+                jQuery(function($) {
+                  $(document).ready( function() {
+                    //enabling stickUp on the '.navbar-wrapper' class
+                    $('.navbar-wrapper').stickUp({
+                      //enabling marginTop with the 'auto' setting 
+                      marginTop: 'auto'
+                    });
+                });
+              });
+              
+              </script>            
+            </xmp>
+          <p></p>
+          <p class="lead">
+            其工作过程为，stickUp插件自动获取 '.navbar-wrapper' 的上边距值，并在滚动过程中赋予目标元素。
+          </p>
+        </div>
+        <div class="col-md-5">
+        </div>
+      </div> <!-- END EXTRAS -->
+
+      <!-- START THE FOOTER -->
+      <footer id="contact" class="row featurette">
+        <div class="row">
+        	
+          <span class="text-center">
+          	<h3 class="featurette-heading">APP 版发布！ <small><b><i>Nov 1st, 2014</i></b></small></h3>
+          	<h3 class="featurette-heading">ipaikt</h3><h4>一个 课程表和笔记结合的APP应用</h4></span><br>
+          <div class="text-center col-sm-12 col-lg-12">
+            <p>© 2013 Liran Cohen |  <a href="http://www.ipaikt.com">爱拍课堂</a></p>
+            
+          </div>
+        </div>
+      </footer> <!-- END FOOTER -->
+
+    </div> <!-- END CONTENT -->
+
+
+    <!-- Bootstrap & Plugins core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+    <script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://cdn.bootcss.com/twitter-bootstrap/3.0.2/js/bootstrap.min.js"></script>
+
+    <!-- ### IMPORTANT ### stickUp javascript file -->
+    <!-- ######################################### --> 
+    <script src="http://www.bootcss.com/p/stickup/js/stickUp.min.js"></script>
+    <!-- ######################################### -->
+
+    <!-- ### IMPORTANT ### stickUp options in use -->
+    <!-- ######################################## --> 
+    <script type="text/javascript">
+      jQuery(function($) {
+        $(document).ready( function() {
+          $('.navbar-wrapper').stickUp({
+                        parts: {
+                          0:'home',
+                          1:'features',
+                          2: 'updates',
+                          3: 'installation',
+                          4: 'one-pager',
+                          5: 'extras',
+                          6: 'contact'
+                        },
+                        itemClass: 'menuItem',
+                        itemHover: 'active',
+                        topMargin: 'auto'
+                      });
+        });
+      });
+    </script>
+    <!--<script src="http://www.bootcss.com/p/projects.js"></script>
+    <script src=" http://hm.baidu.com/h.js?3d8e7fc0de8a2a75f2ca3bfe128e6391" type="text/javascript"></script>-->
+    <!-- ######################################## -->
 </body>
-
-<!-- 自定义 游客访问  操作 Javascript 文件 -->
-<script src="__JS__/user_custom.js"></script>
 </html>
