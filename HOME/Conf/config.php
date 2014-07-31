fcebc416a98d508b0b4e0638083230cb0cb8b5aa
fanfzj1234 <fanfzj@163.com>
2014-07-31 17:35:37 +0800

    配置文件修改 2014/7/31

--- HOME/Conf/config.php
+++ HOME/Conf/config.php
@@ -2,13 +2,12 @@
 return array(
 	//'配置项'=>'配置值'
 
-	    'SHOW_PAGE_TRACE'=>true,//开启页面trace
+	    'SHOW_PAGE_TRACE'=>false,//开启页面trace
 		'URL_HTML_SUFFIX'=>'xml|html|shtml',//配置伪静态
 		'URL_CASE_INSENSITIVE' =>true,//url大小写
 		
 		// 'URL_ROUTER_ON' => true, //URL路由
-		
-         'URL_MODEL' => '1',//去掉index.php
+		 'URL_MODEL' => '3',//去掉index.php
 		
 		'OUTPUT_ENCODE'=>false,
 
