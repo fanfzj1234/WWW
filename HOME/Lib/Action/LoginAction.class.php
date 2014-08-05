6cdfc6c32f2fd98707336e888feb1a3f8aae0f43
fanfzj1234 <fanfzj@163.com>
2014-08-05 09:53:15 +0800

    控制器更新

--- HOME/Lib/Action/LoginAction.class.php
+++ HOME/Lib/Action/LoginAction.class.php
@@ -2,13 +2,12 @@
     class LoginAction extends CommonAction{
 		
 	    /*实现用户登陆*/
-		public function user_register(){
-			$type=$_POST['type'];
-			if($type=="normal_student")
-			{
-				$studentId=$_POST['studentId'];
-				$password=$_POST['student_password'];
-				$schoolId=$_POST['schoolId'];
+	    public function user()
+		{
+			$studentId="2012021039";
+				$password="921128";
+				$schoolId="齐齐哈尔大学";
+				$type="normal_student";
 				
 				$data['A']="User_Register";
 				$data['AC']="register";
@@ -21,8 +20,41 @@
 				$data['type']=$type;  
                 $data['studentId']=$studentId;  
 				$data['password']=$password;  
-				$data['schoolId']=$schoolId;  
+				$data['schoolId']=$schoolId;
+				
                 $data_json=json_encode($data);
+				$json="json=".$data_json;  
+			    $url = "http://api.ipaikt.com:88";
+				$json_data=http_post_data($url,$json);
+				var_dump($json_data);
+				
+		}
+		public function user_register(){
+			$type=$_POST['type'];
+			
+			//$type="normal_student";
+			if($type=="normal_student")
+			{
+				//$studentId="2012021039";
+				//$password="921128";
+				$schoolId="齐齐哈尔大学";
+				$studentId=$_POST['studentId'];
+				$password=$_POST['student_password'];
+				//$schoolId=$_POST['schoolId'];
+				$type="normal_student";
+				$data['A']="User_Register";
+				$data['AC']="register";
+				$data['C']=base64_encode(gzencode("http".time()."W"));  
+				$data['E']=1;
+				$data['V'][]="type";  
+                $data['V'][]="studentId";  
+				$data['V'][]="password";  
+				$data['V'][]="schoolId";  
+				$data['type']=$type;  
+                $data['studentId']=$studentId;  
+				$data['password']=$password;  
+				$data['schoolId']=$schoolId;  
+				
 				
 				
 			}
@@ -50,7 +82,7 @@
 				$data['password']=$password;  
 				$data['password_again']=$password_again;
 				$data['schoolId']=$schoolId;  
-                $data_json=json_encode($data);
+               
 
 			}
 			if($type=="custom")
@@ -62,13 +94,14 @@
 				$data['E']=1;
                 $data['V'][]="type";  
 				$data['type']=$type;  
-                $data_json=json_encode($data);
+                
 
 			}
-			
-			    $url="";
-				$json_data=Common/http_post_data($url,$data_json);
-				return $json_data;
+			    $data_json=json_encode($data);
+				$json="json=".$data_json;  
+			    $url = "http://api.ipaikt.com:88";
+				$json_data=http_post_data($url,$json);
+				echo $json_data;
 		}
 		/*注销用户操作*/
 		public function  do_logout(){
